660d097c1e77a3df114b98c646aecbadf65edf43
rlw92 <r_l_williams@yahoo.com>
2022-09-03 16:36:59 +0100

    Beefed up the personal info section, time to move onto creating a loadout example of the information given and possibly have to group all personal information into an array object

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -7,28 +7,30 @@ import './App.css'
 export class App extends Component {
   constructor(props) {
     super(props)
-  
+
+    this.state={
+      PIname:""
+    }
+
+    this.pullData=this.pullData.bind(this)
+
   }
+
+  pullData(data) {
+    this.setState({
+      PIname:data
+    })  }
 
   render() {
     return (
       <div className="skltn">
         <h1 style={{textAlign:"center"}}>CV Builder</h1>
-          <Personalinfo />
+          <Personalinfo func={this.pullData}/>
           <Education />
           <Employment />
-        
-      {/* } {this.state.shwMdl  &&  <Modal hide={this.hideModal}/>  } 
-        <Header title="Personal Information" edit={this.showModal} info={this.state.PI} fnct={this.chngePI}/>
-        
-        
-        <Header title="About Me" edit={this.showModal} info={this.state.about} fnct={this.chngeAbout}/>
-        <Header title="Employment History" edit={this.showModal} info={this.state.emplyHis}/>
-        <Header title="Education" edit={this.showModal} info={this.state.education}/>
-        <Header title="Contact" edit={this.showModal} info={this.state.contact}/>
-        
-        */ }
-        
+     
+
+
 
       </div>
     )
--- cv-project/src/components/Personalinfo.js
+++ cv-project/src/components/Personalinfo.js
@@ -6,87 +6,111 @@ constructor(props) {
   super(props)
 
   this.state = {
-      edit: "i am rhys",
-     shwMdl: false,
-     PI:"i am rhys"
-     
+      shwMdl: false,
+     about:"i am rhys",
+     name:"Rhys",
+     address:"",
+     cntct:""
+
   }
 
- 
+
       this.showModal = this.showModal.bind(this)
       this.hideModal = this.hideModal.bind(this)
-      this.editInfo = this.editInfo.bind(this)
+      this.editAbout = this.editAbout.bind(this)
+      this.editName=this.editName.bind(this)
+      this.editAddress=this.editAddress.bind(this)
+      this.editCont=this.editCont.bind(this)
       this.onSubmitTask = this.onSubmitTask.bind(this)
 
 
 }
+
+
+
       showModal (){
         this.setState({
           shwMdl:true
         })
       }
-      
+
       hideModal (){
         this.setState({
           shwMdl:false
         })
       }
 
-      editInfo (e){
-        this.setState(
-          {
-            
-            edit: e.target.value,
-            
-            
-          }
-          
-        )
-      }
+      editAbout (e){this.setState({about: e.target.value,})}
+      editName (e){this.setState({name: e.target.value,})}
+      editAddress(e){this.setState({address: e.target.value,})}
+      editCont(e){this.setState({cntct: e.target.value,})}
+
 
       onSubmitTask = (e) => {
         e.preventDefault();
         this.setState({
-          PI: this.state.edit,
+          about: this.state.about,
+          name:this.state.name,
+          address:this.state.address,
+          cntct: this.state.cntct,
           shwMdl: false
         });
       };
 
   render() {
-      
+
     return (
-        
-      
+
+
         <div>
-            
+
          { this.state.shwMdl  &&   <div className="modal">
-        
+
         <div className="modal-content">
         <span className="close" onClick={this.hideModal}>&times;</span>
         <p>Edit Personal Information Below</p>
         <div className="inputs">
+
+       <p>Name</p>
+        <input
+        onChange={this.editName}
+        value={this.state.name}></input>
+       <p>Address</p>
+        <input
+        onChange={this.editAddress}
+        value={this.state.address}></input>
+       <p>Contact Info</p>
+        <input
+        onChange={this.editCont}
+        value={this.state.cntct}></input>
+      <p>About Me</p>
         <textarea
-        onChange={this.editInfo}
-        value={this.state.edit}
+        onChange={this.editAbout}
+        value={this.state.about}
         type="textarea"></textarea>
+
+
         <button onClick={this.onSubmitTask}>Edit and close</button>
       </div>
       </div>
       </div>
   }
-    
-    
+
+
 
 
         <div className="hdr">
         <h2>Personal Information</h2>
-        
+
         <button onClick={this.showModal}>Edit</button>
         </div>
-        <p>{this.state.PI}</p>
-        
+        <p><strong>Name:</strong> {this.state.name}</p>
+        <p><strong>Address:</strong> {this.state.address}</p>
+        <p><strong>Contact Number:</strong> {this.state.cntct}</p>
+        <p><strong>About:</strong> {this.state.about}</p>
+
         </div>
-        
+
     )
   }
 }
